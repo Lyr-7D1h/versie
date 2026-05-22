a57735c24a34ef4c94da3bde7a011ee66015bc89
Lyr <lyr-7d1h@pm.me>
2026-05-22 14:00:30 +0200

    fix: Fix dts build and upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+import dts from 'unplugin-dts/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ bundleTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
